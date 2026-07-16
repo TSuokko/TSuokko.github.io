ae580d301f90a5854a94657955c99d761e374210
TSuokko <tsuokko@gmail.com>
2026-07-16 21:22:54 +0300

    junk and other items added

--- js/items.js
+++ js/items.js
@@ -11,6 +11,7 @@ export const CATEGORY_COLORS = Object.freeze({
   Meds: "#ffb6c1", //light pink
   Tool: "#2b5d8b", //blue
   Misc: "#221e1e", //Black
+  Junk: "#7721ee", //saddle brown
 });
 
 const DEFAULT_CATEGORY_COLOR = "#718071";
@@ -25,6 +26,8 @@ export const ITEMS = [
   { id: "ballistic-armor", name: "Ballistic Armor", load: 5, requirement: 3, category: "Armor" },
   { id: "steel-armor", name: "Steel Armor", load: 25, requirement: 6, category: "Armor" },
   { id: "clothing", name: "Clothes", load: 2, requirement: 1, category: "Armor" },
+  { id: "ins-clothing", name: "Insulated Clothes", load: 5, requirement: 1, category: "Armor" },
+  { id: "hazmat", name: "Hazmat Suit", load: 10, requirement: 1, category: "Armor" },
   //melee
   { id: "shiv", name: "Shiv", load: 1, requirement: 1, category: "Melee"},
   { id: "knife", name: "Knife", load: 2, requirement: 2, category: "Melee"},
@@ -124,15 +127,17 @@ export const ITEMS = [
   { id: "flare", name: "Flare", load: 1, requirement: 1, category: "Gear"},
   { id: "flashlight", name: "Flashlight", load: 2, requirement: 1, category: "Gear"},
   { id: "gasmask", name: "Gas Mask", load: 12, requirement: 1, category: "Gear"},
-  { id: "lockpick", name: "Lockpick", load: 4, requirement: 1, category: "Gear"},
+  { id: "lockpick", name: "Lockpick", load: 4, requirement: 1, category: "Gear"},  
   { id: "rope", name: "Rope", load: 8, requirement: 1, category: "Gear"},
   { id: "sleeping-bag", name: "Sleeping Bag", load: 10, requirement: 1, category: "Gear"},
+  { id: "tent", name: "Tent", load: 12, requirement: 1, category: "Gear"},
   { id: "weapon-repair-kit", name: "Weapon Repair Kit", load: 10, requirement: 1, category: "Gear"},
   //food
   { id: "canned-food", name: "Canned Food", load: 2, requirement: 1, category: "Food"},
+  { id: "dried-food", name: "Dried Food", load: 1, requirement: 1, category: "Food"},
   { id: "candy", name: "Candy", load: 1, requirement: 1, category: "Food"},
   { id: "boxed-food", name: "Boxed Food", load: 4, requirement: 1, category: "Food"},
-  { id: "vegetables", name: "Vegetable", load: 1, requirement: 1, category: "Food"},
+  { id: "vegetables", name: "Vegetable/Fruit", load: 1, requirement: 1, category: "Food"},
   { id: "meat", name: "Meat", load: 3, requirement: 1, category: "Food"},
   { id: "steak", name: "Steak", load: 6, requirement: 1, category: "Food"},
   { id: "drink", name: "Drink", load: 2, requirement: 1, category: "Food"},
@@ -140,6 +145,7 @@ export const ITEMS = [
   { id: "stew", name: "Stew", load: 8, requirement: 1, category: "Food"},
   //meds
   { id: "antibiotics", name: "Antibiotics", load: 1, requirement: 1, category: "Meds"},
+  { id: "antivenom", name: "Antivenom", load: 3, requirement: 1, category: "Meds"},
   { id: "doctors-bag", name: "Doctor's Bag", load: 15, requirement: 1, category: "Meds"},
   { id: "first-aid-kit", name: "First Aid Kit", load: 4, requirement: 1, category: "Meds"},
   { id: "rad-away", name: "Rad-Away", load: 4, requirement: 1, category: "Meds"},
@@ -149,21 +155,53 @@ export const ITEMS = [
   { id: "chem", name: "Chem (Bottle)", load: 1, requirement: 1, category: "Meds"},
   { id: "chems", name: "Chems (10x Pills)", load: 1, requirement: 1, category: "Meds"},
   { id: "overclock-program", name: "Overclock Program 10x", load: 1, requirement: 1, category: "Meds"},
+  { id: "cigs", name: "Cigarettes 10x", load: 1, requirement: 1, category: "Meds"},
+  //junk
+  { id: "ad-junk", name: "Adhesive Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "ac-junk", name: "Acid Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "al-junk", name: "Aluminum Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "ans-junk", name: "Antiseptic Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "asb-junk", name: "Asbestos Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "bf-junk", name: "Ballistic Fiber Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "ce-junk", name: "Ceramic Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "cir-junk", name: "Circuitry Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "cir-junk", name: "Circuitry Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "cl-junk", name: "Cloth Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "cr-junk", name: "Crystal Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "cu-junk", name: "Copper Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "fer-junk", name: "Fertilizer Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "fio-junk", name: "Fiber Optics Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "fig-junk", name: "Fiber Glass Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "ge-junk", name: "Gears Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "gl-junk", name: "Glass Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "led-junk", name: "Lead Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "let-junk", name: "Leather Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "nuc-junk", name: "Nuclear Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "oil-junk", name: "Oil Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "pa-junk", name: "Paint Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "pl-junk", name: "Plastic Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "rb-junk", name: "Rubber Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "scr-junk", name: "Screw Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "sil-junk", name: "Silver Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "spr-junk", name: "Spring Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "stl-junk", name: "Steel Junk", load: 1, requirement: 1, category: "Junk"},
+  { id: "wd-junk", name: "Wood Junk", load: 1, requirement: 1, category: "Junk"},
   //misc
-  { id: "computer", name: "Computer Boy", load: 4, requirement: 1, category: "Misc"},
+  { id: "computer", name: "Pip Boy", load: 4, requirement: 1, category: "Misc"},
   { id: "stealth-boy", name: "Stealth Boy", load: 3, requirement: 1, category: "Misc"},
   { id: "two-way-radio", name: "Two-Way Radio", load: 2, requirement: 1, category: "Misc"},
   { id: "book", name: "Book", load: 1, requirement: 1, category: "Misc"},
     { id: "Load1", name: "Other1", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load2", name: "Other2", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load3", name: "Other3", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load4", name: "Other4", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load5", name: "Other5", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load6", name: "Other6", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load7", name: "Other7", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load8", name: "Other8", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load9", name: "Other9", load: 1, requirement: 1, category: "Misc"},
-    { id: "Load10", name: "Other10", load: 1, requirement: 1, category: "Misc"}
+    { id: "Load2", name: "Other2", load: 2, requirement: 1, category: "Misc"},
+    { id: "Load3", name: "Other3", load: 3, requirement: 1, category: "Misc"},
+    { id: "Load4", name: "Other4", load: 4, requirement: 1, category: "Misc"},
+    { id: "Load5", name: "Other5", load: 5, requirement: 1, category: "Misc"},
+    { id: "Load6", name: "Other6", load: 6, requirement: 1, category: "Misc"},
+    { id: "Load7", name: "Other7", load: 7, requirement: 1, category: "Misc"},
+    { id: "Load8", name: "Other8", load: 8, requirement: 1, category: "Misc"},
+    { id: "Load9", name: "Other9", load: 9, requirement: 1, category: "Misc"},
+    { id: "Load10", name: "Other10", load: 10, requirement: 1, category: "Misc"}
+
 ];
 
 export function categoryColor(category) {
